48635111f0b01d1e5ab7e1735d98feda63a79fc7
Gurkirat Singh <tbhaxor@gmail.com>
2024-03-21 02:26:45 +0530

    Remove app service from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { AccountsModule } from "./accounts/accounts.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { AuthModule } from "./auth/auth.module";
 import { YoutubeModule } from "./youtube/youtube.module";
-import { AwsService } from "./clients/aws.service";
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 
 @Module({
   imports: [AccountsModule, PrismaModule, AuthModule, YoutubeModule],
-  providers: [AwsService, AppService],
+  providers: [AppService],
   controllers: [AppController],
 })
 export class AppModule {}
