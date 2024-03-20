841df8d9c0ae2d74fde208fe8e25f950abb9f1ff
Gurkirat Singh <tbhaxor@gmail.com>
2024-03-21 02:25:55 +0530

    Configure AWS module

--- src/clients/aws.service.ts
+++ src/clients/aws.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@nestjs/common";
-import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 @Injectable()
 export class AwsService {
-  getS3Client(): S3Client {
-    return new S3Client({
+  private s3Client: S3Client;
+
+  constructor() {
+    this.s3Client = new S3Client({
       region: process.env.AWS_REGION,
       endpoint: process.env.AWS_ENDPOINT,
       credentials: {
@@ -12,5 +15,18 @@ export class AwsService {
         secretAccessKey: process.env.AWS_SECRET_KEY,
       },
     });
+  }
+
+  get client() {
+    return this.s3Client;
+  }
+
+  getSignedUrlForUpload(key: string) {
+    const cmd = new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET,
+      Key: key,
+      ACL: "private",
+    });
+    return getSignedUrl(this.s3Client, cmd, { expiresIn: 3600 });
   }
 }
