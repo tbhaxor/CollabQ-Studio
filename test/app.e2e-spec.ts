c7f6c78719edf417bde9b176296cb6b202d7a255
Gurkirat Singh <tbhaxor@gmail.com>
2024-03-21 20:46:26 +0530

    Use `test` in app.e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,11 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { INestApplication } from "@nestjs/common";
-import { AppModule } from "../src/app.module";
-import * as request from "supertest";
-import { faker } from "@faker-js/faker";
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import * as request from 'supertest';
+import { faker } from '@faker-js/faker';
 
 const name = faker.person.firstName();
-describe("AppController (/app)", () => {
+describe('AppController (E2E)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -17,11 +17,11 @@ describe("AppController (/app)", () => {
     await app.init();
   });
 
-  it("/hello (GET)", () => {
-    return request(app.getHttpServer()).get("/app/hello").expect(200).expect("Hello World!");
+  test('/hello (GET)', () => {
+    return request(app.getHttpServer()).get('/app/hello').expect(200).expect('Hello World!');
   });
 
-  it(`/hello?name=${name} (GET)`, () => {
+  test(`/hello?name=${name} (GET)`, () => {
     return request(app.getHttpServer()).get(`/app/hello?name=${name}`).expect(200).expect(`Hello ${name}!`);
   });
 });
